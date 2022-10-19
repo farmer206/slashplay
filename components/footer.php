8d70b9688f6ed5dadf023f42fba5288c5f3b52e1
farmer206 <farmer206@protonmail.com>
2022-10-19 03:15:57 +0300

    Change footer section

--- components/footer.php
+++ components/footer.php
@@ -1,34 +1,85 @@
 <footer class="footer">
     <div class="footer__top">
-        <div class="footer__subscribe container">
-            <div class="footer__social social">
-                <a href="#" class="social__item">
-                    <img src="img/icons/footer/telegram.svg" alt="Telegram">
-                </a>
-                <a target="_blank" href="https://www.youtube.com/channel/UCkKVdpVWu8r3rE_sNlhnFjg" class="social__item">
-                    <img src="img/icons/footer/youtube.svg" alt="Youtube">
-                </a>
-                <a target="_blank" href="https://vk.com/slashplay_ptz" class="social__item">
-                    <img src="img/icons/footer/vk.svg" alt="ВКонтакте">
-                </a>
+        <div class="footer__main container">
+            <div class="footer__row">
+                <div class="footer__column">
+                    <div class="footer__label">Company Info</div>
+                    <nav class="footer__menu menu-footer">
+                        <ul class="menu-footer__list">
+                            <li class="menu-footer__item"><a href="#" class="menu-footer__link">About us</a></li>
+                            <li class="menu-footer__item"><a href="#" class="menu-footer__link">Carrier</a></li>
+                            <li class="menu-footer__item"><a href="#" class="menu-footer__link">We are hiring</a></li>
+                            <li class="menu-footer__item"><a href="#" class="menu-footer__link">Blog</a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div class="footer__column">
+                    <div class="footer__label">Legal</div>
+                    <nav class="footer__menu menu-footer">
+                        <ul class="menu-footer__list">
+                            <li class="menu-footer__item"><a href="#" class="menu-footer__link">About us</a></li>
+                            <li class="menu-footer__item"><a href="#" class="menu-footer__link">Carrier</a></li>
+                            <li class="menu-footer__item"><a href="#" class="menu-footer__link">We are hiring</a></li>
+                            <li class="menu-footer__item"><a href="#" class="menu-footer__link">Blog</a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div class="footer__column">
+                    <div class="footer__label">Features</div>
+                    <nav class="footer__menu menu-footer">
+                        <ul class="menu-footer__list">
+                            <li class="menu-footer__item"><a href="#" class="menu-footer__link">Business Marketing</a></li>
+                            <li class="menu-footer__item"><a href="#" class="menu-footer__link">User Analytic</a></li>
+                            <li class="menu-footer__item"><a href="#" class="menu-footer__link">Live Chat</a></li>
+                            <li class="menu-footer__item"><a href="#" class="menu-footer__link">Unlimited Support</a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div class="footer__column">
+                    <div class="footer__label">Resourses</div>
+                    <nav class="footer__menu menu-footer">
+                        <ul class="menu-footer__list">
+                            <li class="menu-footer__item"><a href="#" class="menu-footer__link">ISO & Android</a></li>
+                            <li class="menu-footer__item"><a href="#" class="menu-footer__link">Watch a Demo</a></li>
+                            <li class="menu-footer__item"><a href="#" class="menu-footer__link">Customers</a></li>
+                            <li class="menu-footer__item"><a href="#" class="menu-footer__link">API</a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div class="footer__column">
+                    <div class="footer__label">Get in Touch</div>
+                    <div class="footer__contacts contacts-footer">
+                        <a href="#" class="contacts-footer__item contacts-footer__item_phone">+7(911)407-36-68</a>
+                        <a href="#" class="contacts-footer__item contacts-footer__item_map">ул.Максима Горького 25, 4 этаж</a>
+                        <a href="#" class="contacts-footer__item contacts-footer__item_email">test@test.com</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
     <div class="footer__bottom">
         <div class="footer__container container">
-            <div class="footer__privacy">
-                <div class="">&copy; 2022 Все права защищены</div>
-                <div class="footer__menu">
-                    <!-- <a href="#">Политика конфиденциальности</a> -->
-                </div>
+            <div class="footer__copy">&copy; 2022 Все права защищены</div>
+            <div class="footer__social social">
+                <a href="#" class="social__item">
+                    <img src="img/icons/footer/telegram.svg" alt="Telegram">
+                </a>
+                <a class="social__item" target="_blank" href="https://www.youtube.com/channel/UCkKVdpVWu8r3rE_sNlhnFjg">
+                    <img src="img/icons/footer/youtube.svg" alt="Youtube">
+                </a>
+                <a class="social__item" target="_blank" href="https://vk.com/slashplay_ptz">
+                    <img src="img/icons/footer/vk.svg" alt="ВКонтакте">
+                </a>
             </div>
-            <address class="footer__author">Made With Love:
-                <a href="https://t.me/farmer206th"> @farmer206th</a>
-            </address>
         </div>
     </div>
 </footer>
+
 <div class="up-button">
     <a href="#"><img src="/img/icons/footer/up-arrow.svg" alt="Вверх"></a>
 </div>
